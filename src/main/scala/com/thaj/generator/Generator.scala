3fb4493f5c4a836326339f8a5366a7893562d965
Afsal Thaj <afsal.taj06@gmail.com>
2018-05-23 14:57:31 +1000

    Simplified combinator

--- src/main/scala/com/thaj/generator/Generator.scala
+++ src/main/scala/com/thaj/generator/Generator.scala
@@ -12,12 +12,11 @@ trait Zero[State] {
   def zero: State
 }
 
-trait Generator[State, A] extends Zero[State] { self =>
+trait Generator[State, A] { self =>
   def next: State => Option[(State, A)]
   // In this way you can start with a generator service for a single component and chain across
   def map[B](f: A => B): Generator[State, B] = {
     new Generator[State, B] {
-      override def zero: State = self.zero
       override def next: State => Option[(State,B)] = s => {
         for {
           ss <- self.next(s)
@@ -28,7 +27,6 @@ trait Generator[State, A] extends Zero[State] { self =>
 
   def flatMap[B](f: A => Generator[State, B]): Generator[State, B] =
     new Generator[State, B] {
-      override def zero: State = self.zero
       override def next: State => Option[(State, B)] = s => {
         val ss: Option[(State, A)] = self.next(s)
         ss.flatMap(t => f(t._2).next(t._1))
@@ -39,37 +37,39 @@ trait Generator[State, A] extends Zero[State] { self =>
     self.flatMap(bb => b.map(cc => f(bb, cc)))
 
   // Ex: GeneratorService[A, B].run(1000)(a => sendEventHub(a).map(_ => log("sent data"))(identity)
-  def run[F[_]: Monad, E](delay: Long)(sideEffect: A => F[E \/ Unit])(f: State => A): F[\/[E, Unit]] =
-    Generator.unfoldM(this.zero)(delay)(this.next, f)(sideEffect)
+  def run[F[_]: Monad, E](delay: Long)(sideEffect: A => F[E \/ Unit])(implicit m: Zero[State]): F[\/[E, Unit]] =
+    Generator.unfoldM(m.zero)(delay)(this.next)(sideEffect)
 }
 
 object Generator {
   def apply[A, B](implicit instance: Generator[A, B]): Generator[A, B] =
     instance
 
-  def unit[State : Zero, A](a: => A): Generator[State, A] =
+  def unit[State, A](a: => A): Generator[State, A] =
     new Generator[State, A] {
-      override def zero: State = implicitly[Zero[State]].zero
       override def next: State => Option[(State, A)] = s => Some((s, a))
     }
 
   // multiple generator services of S -> A , can be converted to S -> List[A] allowing you
   // to do things like batch insert.
-  def sequence[S : Zero, A](x: List[Generator[S, A]]): Generator[S, List[A]] =
+  def sequence[S, A](x: List[Generator[S, A]]): Generator[S, List[A]] =
   x.foldLeft(Generator.unit[S, List[A]](List[A]()))((acc, a) => a.map2(acc)(_ :: _))
 
   // A seamless finite/infinite data gen
-  private def unfoldM[S, A, E, F[_]: Monad](z: S)(delay: Long)(f: S => Option[(S, A)], g: S => A)(sideEffect: A => F[E \/ Unit]): F[E \/ Unit] = {
-    EitherT(sideEffect(g(z))).foldM(
-      _.left[Unit].pure[F],
-      _ => f(z) match {
-        case Some((n, _)) =>
-          // Throttle the generation and side effecty things!
+  private def unfoldM[S, A, E, F[_]: Monad](z: S)(delay: Long)(f: S => Option[(S, A)])(sideEffect: A => F[E \/ Unit]): F[E \/ Unit] = {
+    f(z) match {
+      case Some((state, value)) => EitherT(sideEffect(value)).foldM(
+        _.left[Unit].pure[F],
+        _ => {
+          // Haha, who cares!
           Thread.sleep(delay)
-          unfoldM[S, A, E, F](n)(delay)(f, g)(sideEffect)
-        case None =>
-          println("Finished sending the data!").right[E].pure[F]
-      }
-    )
+          unfoldM[S, A, E, F](state)(delay)(f)(sideEffect)
+        }
+      )
+
+      case None =>
+        // Done with accepting functions as arguments, let me print - enough is enough!
+        println("Finished sending the data!").right[E].pure[F]
+    }
   }
 }
--- src/main/scala/com/thaj/generator/examples/Simple.scala
+++ src/main/scala/com/thaj/generator/examples/Simple.scala
@@ -1,6 +1,9 @@
 package com.thaj.generator.examples
 
-import com.thaj.generator.Generator
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function
+
+import com.thaj.generator.{Generator, Zero}
 
 import scala.concurrent.Future
 import scalaz.{Monad, \/}
@@ -13,20 +16,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * involving state.
   */
 object Simple {
+
+  private val atomicReference = new AtomicReference[List[Int]]()
+
   implicit val monad: Monad[Future] = new Monad[Future] {
     override def bind[A, B](fa: Future[A])(f: (A) => Future[B]): Future[B] = fa.flatMap(f)
     override def point[A](a: => A): Future[A] = Future { a }
   }
 
-  implicit val generator: Generator[Int, Int] = new Generator[Int, Int] {
-    override def next: (Int) => Option[(Int, Int)] = s =>
-      (s < 10).option((s + 1, s + 1))
+  implicit val zero: Zero[Int] = new Zero[Int] {
     override def zero: Int = 0
   }
 
+  implicit val generator: Generator[Int, Int] = new Generator[Int, Int] {
+    override def next: (Int) => Option[(Int, Int)] = s =>
+      (s < 10).option((s + 1, s + 1))
+  }
+
   def main(args: Array[String]): Unit = {
-    Generator[Int, Int].run[Future, Throwable](100){t => Future[Throwable \/ Unit] {
-      println(s"In thread: ${Thread.currentThread().getName}: value $t").right[Throwable]
-    }}{identity}
+    Generator[Int, Int].run[Future, Throwable](1){s => Future[Throwable \/ Unit] {
+      atomicReference.getAndUpdate(new function.UnaryOperator[List[Int]] {
+        override def apply(t: List[Int]): List[Int] = t :+ s
+      }).right[Throwable].map(_ => ())}
+    } foreach(_ => println(atomicReference.get()))
   }
 }
