260a58b355bc2984f58b54b62be1674e3a42e9db
Afsal Thaj <afsal.taj06@gmail.com>
2018-06-22 00:26:32 +1000

    Update Generator.scala

--- src/main/scala/com/thaj/generator/Generator.scala
+++ src/main/scala/com/thaj/generator/Generator.scala
@@ -18,15 +18,9 @@ object Generator {
         Stream.eval[F, (S, A)](F.delay((state, value))) ++ asFs2Stream[F, S, A](state, delay)(f)
     })
 
-  /**
-    * Generate a batch of data
-    */
   def run[F[_]: Effect, S, A](gens: Generator[S, A]*)(f: A => F[Unit], maxRate: Int = 4): F[Unit] =
     Fs2PublisherSubscriber.withQueue(gens.map(_.asFs2Stream[F].map(_._2)).reduce(_ merge _), f, maxRate).compile.drain
 
-  /**
-    * Generate a batch of data asynchronously, and process them asynchronously.
-    */
   def runBatch[F[_], S, A](n: Int, gens: Generator[S, A]*)(f: List[A] => F[Unit], maxRate: Int = 4)(implicit F: Effect[F]): F[Unit] =
     Fs2PublisherSubscriber.withQueue(
       gens.map(t => {
@@ -40,12 +34,8 @@ object Generator {
         })
     }.filter(_.nonEmpty)).reduce(_ merge _), f, maxRate).compile.drain
 
-  /**
-    * Synchronous emission and processing of data
-    */
   def runSync[F[_]: Effect, S, A](gens: Generator[S, A]*)(f: A => F[Unit]): F[Unit] =
     gens.map(_.asFs2Stream[F].map(_._2)).reduce(_ merge _).evalMap(f).compile.drain
-
 
   implicit class GeneratorWithZeroOps[S, A](g: Generator[S, A]) {
     def withDelay(n: Int): Generator[S, A] = g.copy(delay = Some(n))
