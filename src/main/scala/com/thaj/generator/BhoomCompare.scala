4bc728112178b5804ee8c4c7e23fd591a43c6195
afsaltthaj <afsal.taj06@gmail.com>
2018-08-12 14:21:17 +1000

    Rename parameters

--- src/main/scala/com/thaj/generator/BhoomCompare.scala
+++ src/main/scala/com/thaj/generator/BhoomCompare.scala
@@ -9,143 +9,118 @@ import com.thaj.generator.Delta.Meta
 import Delta._
 
 
-trait FindMeta[A] {
-  def findChangedParams(a: A, b: A): Delta.Meta
+trait FindDeltaMeta[A] {
+  def apply(a: A, b: A): Delta.Meta
 }
 
-object FindMeta extends LowPriorityInstances0 {
-  def apply[A](implicit ev: FindMeta[A]): FindMeta[A] = ev
-
-  def toKey(raw: String): String = {
-    val str = new StringBuilder
-    var lastLower = false
-    raw.foreach { c =>
-      if (c.isLower) {
-        lastLower = true
-        str += c
-      } else {
-        if (lastLower) str += '-'
-        lastLower = false
-        str += c.toLower
-      }
-    }
-    str.toString
-  }
+object FindDeltaMeta extends LowPriorityInstances0 {
+  def apply[A](implicit ev: FindDeltaMeta[A]): FindDeltaMeta[A] = ev
 }
 
 trait LowPriorityInstances0 extends LowPriorityInstances1 {
-  implicit def hNilNamer: FindMeta[HNil] =
-    new FindMeta[HNil] {
-      def findChangedParams(a: HNil, b: HNil): Delta.Meta = Delta.Meta.empty
-    }
+  implicit def hNilNamer: FindDeltaMeta[HNil] =
+    (_, _) => Delta.Meta.empty
 
-  // Needn't rely unused implicits warnings by compiler; its a lie
   implicit def hListThatCanHaveFOfGOfAnyVal[F[_], G[_], A, B, K <: Symbol, T <: HList](
     implicit
     witness: Witness.Aux[K],
     IsList: A Is F[G[B]],
     IsAnyVal: B As AnyVal,
-    D: Lazy[FindMeta[T]],
-  ): FindMeta[FieldType[K, A] :: T] =
-    new FindMeta[FieldType[K, A] :: T] {
-      override def findChangedParams(a: FieldType[K, A] :: T, b: FieldType[K, A] :: T): Delta.Meta =
-        (if (a.head == b.head)
-          Meta.empty
-        else
-          Meta(witness.value.name, (a.head: A).toString, (b.head: A).toString)) ++ D.value.findChangedParams(a.tail, b.tail)
-    }
+    D: Lazy[FindDeltaMeta[T]],
+  ): FindDeltaMeta[FieldType[K, A] :: T] =
+    (a, b) =>
+      (if (a.head == b.head)
+        Meta.empty
+      else
+        Meta(witness.value.name, (a.head: A).toString, (b.head: A).toString)) ++ D.value.apply(a.tail, b.tail)
 
-  implicit def namerA[A, R <: HList](implicit E: LabelledGeneric.Aux[A, R], D: FindMeta[R]): FindMeta[A] = {
-    new FindMeta[A] {
-      override def findChangedParams(a: A, b: A): Meta = D.findChangedParams(E.to(a), E.to(b))
-    }
+  implicit def findDiffA[A, R <: HList](implicit E: LabelledGeneric.Aux[A, R], D: FindDeltaMeta[R]): FindDeltaMeta[A] = {
+    (a, b) => D.apply(E.to(a), E.to(b))
   }
 }
-
 
 trait LowPriorityInstances1 extends LowPriorityInstances2 {
   implicit def hListWithSimpleAnyVal[A, K <: Symbol, T <: HList](
     implicit
     witness: Witness.Aux[K],
     IsAnyVal: A As AnyVal,
-    D: Lazy[FindMeta[T]],
-  ): FindMeta[FieldType[K, A] :: T] =
+    D: Lazy[FindDeltaMeta[T]],
+  ): FindDeltaMeta[FieldType[K, A] :: T] =
     (a, b) =>
       (if (a == b)
         Meta.empty
       else
-        Meta(witness.value.name, (a.head: A).toString, (b.head: A).toString)) ++ D.value.findChangedParams(a.tail, b.tail)
+        Meta(witness.value.name, (a.head: A).toString, (b.head: A).toString)) ++ D.value.apply(a.tail, b.tail)
 }
-
 
 trait LowPriorityInstances2 extends LowPriorityInstances3 {
   implicit def hLIstWithFofHListInsideIt[A, K <: Symbol, H, InnerT <: HList, T <: HList](
-     implicit
-     witness: Witness.Aux[K],
-     IsList: H As List[A],
-     eachH: LabelledGeneric.Aux[A, InnerT],
-     D: Lazy[FindMeta[T]],
-     E: Lazy[FindMeta[InnerT]]
-  ): FindMeta[FieldType[K, H] :: T] =
-    new FindMeta[FieldType[K, H] :: T] {
-      override def findChangedParams(a: FieldType[K, H] :: T, b: FieldType[K, H] :: T) : Delta.Meta = {
-        val r: Seq[(A, A)] = a.head.asInstanceOf[List[A]].flatMap(aa => b.head.asInstanceOf[List[A]].map(bb => (aa, bb)))
-        val r2 = { r.map {case (aa, bb) => E.value.findChangedParams(eachH.to(aa), eachH.to(bb))} }
-        r2.reduce(_ ++ _).mapKeys(t => witness.value.name + "." +t ) ++ D.value.findChangedParams(a.tail, b.tail)
-      }
+    implicit
+    witness: Witness.Aux[K],
+    IsList: H As List[A],
+    eachH: LabelledGeneric.Aux[A, InnerT],
+    D: Lazy[FindDeltaMeta[T]],
+    E: Lazy[FindDeltaMeta[InnerT]]
+  ): FindDeltaMeta[FieldType[K, H] :: T] =
+    (a, b) => {
+      val r: Seq[(A, A)] = a.head.asInstanceOf[List[A]].flatMap(aa => b.head.asInstanceOf[List[A]].map(bb => (aa, bb)))
+      val r2 = { r.map {case (aa, bb) => E.value.apply(eachH.to(aa), eachH.to(bb))} }
+      r2.reduce(_ ++ _).mapKeys(t => witness.value.name + "." +t ) ++ D.value.apply(a.tail, b.tail)
     }
 }
 
 trait LowPriorityInstances3 extends LowPriorityInstances4 {
   implicit def hListNamerWithHListInsideOfInsideOf[K <: Symbol, H, InnerT <: HList, T <: HList](
-     implicit
-     witness: Witness.Aux[K],
-     eachH: LabelledGeneric.Aux[H, InnerT],
-     D: Lazy[FindMeta[T]],
-     E: Lazy[FindMeta[InnerT]]
-  ): FindMeta[FieldType[K, H] :: T] =
-    new FindMeta[FieldType[K, H] :: T] {
-      override def findChangedParams(a: FieldType[K, H] :: T, b: FieldType[K, H] :: T) : Delta.Meta = {
-       E.value.findChangedParams(eachH.to(a.head.asInstanceOf[H]),
+    implicit
+    witness: Witness.Aux[K],
+    eachH: LabelledGeneric.Aux[H, InnerT],
+    D: Lazy[FindDeltaMeta[T]],
+    E: Lazy[FindDeltaMeta[InnerT]]
+  ): FindDeltaMeta[FieldType[K, H] :: T] =
+    (a, b) =>
+       E.value.apply(eachH.to(a.head.asInstanceOf[H]),
          eachH.to(b.head.asInstanceOf[H])).mapKeys(t => witness.value.name + "." +t) ++
-         D.value.findChangedParams(a.tail, b.tail)
-      }
-    }
+         D.value.apply(a.tail, b.tail)
 }
 
 trait LowPriorityInstances4 {
   implicit def simpleHList[A, K <: Symbol, H, T <: HList](
        implicit
        witness: Witness.Aux[K],
-       D: Lazy[FindMeta[T]]
-  ): FindMeta[FieldType[K, H] :: T] =
-    new FindMeta[FieldType[K, H] :: T] {
-      override def findChangedParams(a: FieldType[K, H] :: T, b: FieldType[K, H] :: T) : Delta.Meta =
+       D: Lazy[FindDeltaMeta[T]]
+  ): FindDeltaMeta[FieldType[K, H] :: T] =
+    new FindDeltaMeta[FieldType[K, H] :: T] {
+      override def apply(a: FieldType[K, H] :: T, b: FieldType[K, H] :: T) : Delta.Meta =
         (if (a.head == b.head)
           Meta.empty
         else
-          Meta(witness.value.name, (a.head: H).toString, (b.head: H).toString)) ++ D.value.findChangedParams(a.tail, b.tail)
+          Meta(witness.value.name, (a.head: H).toString, (b.head: H).toString)) ++ D.value.apply(a.tail, b.tail)
     }
 }
 
 object BlaXX extends App {
-  case class Afsal(dla: String)
-  case class OuterAfsal(afsal: Afsal)
-  case class OuterAfsalList(listAfsal: List[OuterAfsal], x1: String, x2: String)
-
-  val afsal1 = Afsal("")
-  val afsal2 = Afsal("sdasd")
-  val afsal3 = Afsal("afsal")
-
-  val o1 = OuterAfsal(afsal1)
-  val o2 = OuterAfsal(afsal2)
-  val o3 = OuterAfsal(afsal3)
+  case class Blob(name: String, usersPermitted: List[String])
+  case class StorageAccount(blob: Blob, fileNames: List[String])
+  case class Exchange(storageAccounts: List[StorageAccount], sku: String, tier: String)
 
 
-  val o4 = OuterAfsalList(List(o1, o2), "1", "a")
-  val o5 = OuterAfsalList(List(o3), "2", "b")
+  val blob1 = Blob("sdasd", List("afsal", "thaj", "bi"))
+  val blob2 = Blob("asd", List("ds"))
 
-  println(FindMeta[OuterAfsalList].findChangedParams(o4, o5).map(_.shows))
-  // Map(listAfsal.afsal.dla -> (,afsal), x1 -> (1,2), x2 -> (a,b))
+ // println(FindDiff[Blob].apply(blob1, blob2).map(_.shows))
+
+  val blob = Blob("afsal", List("sdasd", "sdadad"))
+
+  val o1 = StorageAccount(blob1, List(""))
+  val o2 = StorageAccount(blob2, List("sad"))
+  val o3 = StorageAccount(blob, List("sdadasd"))
+
+  val o4 = Exchange(List(o1, o2), "1", "a")
+  val o5 = Exchange(List(o3, o2), "2", "b")
+
+  println(FindDeltaMeta[Exchange].apply(o4, o5).sortBy(_.key).map(_.shows))
+
+
+  // There are a few bugs in output, but you get the idea
 
 }
--- src/main/scala/com/thaj/generator/Delta.scala
+++ src/main/scala/com/thaj/generator/Delta.scala
@@ -42,7 +42,7 @@ object Delta {
   object UpdateInfo {
     implicit val updateInfo: Show[UpdateInfo] =
       Show.shows[UpdateInfo](a =>
-        s"{ field: ${a.key}, previous: ${a.previous}, new: ${a.newValue} }"
+        s"{ \n field: ${a.key}, \n previous: ${a.previous}, \n new: ${a.newValue} \n }\n"
       )
   }
 
@@ -63,7 +63,25 @@ object Delta {
       Meta.mapKeys(f, self)
   }
 
-  trait DeltaOp[A]
+  trait DeltaOp[A] { self =>
+    def fold[C](
+      ifNew: A => C,
+      ifChanged: (A, A, Delta.Meta) => C,
+      ifUnchanged: A => C,
+      ifDeleted: A => C
+    ): C =
+      this match {
+        case New(a) => ifNew(a)
+        case Changed(a, b, m) => ifChanged(a, b, m)
+        case Unchanged(a) => ifUnchanged(a)
+        case Deleted(a) => ifDeleted(a)
+      }
+
+    def isNew: Boolean = fold(_ => true, (_, _, _) => false, _ => false, _ => false)
+    def isChanged: Boolean = fold(_ => false, (_, _, _) => true, _ => false, _ => false)
+    def isUnchanged: Boolean = fold(_ => false, (_, _, _) => false, _ => true, _ => false)
+    def isDeleted: Boolean = fold(_ => false, (_, _, _) => false, _ => false, _ => true)
+  }
 
   case class New[A](a: A) extends DeltaOp[A]
   case class Changed[A](a: A, b: A, meta: Meta) extends DeltaOp[A]
@@ -89,14 +107,17 @@ object Delta {
         Changed(a, b, Meta.apply("bla", a.bla.someValue, b.bla.someValue)).left[Unchanged[MajorResource1]])
 
   private def asThese[A](a: List[A], b: List[A])(implicit N: HasName[A]): List[A \&/ A] = {
-   val left =
-     a.map(a => b.find(bb =>
-       N.name(bb) == N.name(a)) match {
-       case None => This(a)
-       case Some(bb) => Both(a, bb)
+    val left =
+     a.map(aa => b.find(bb =>
+       N.name(bb) == N.name(aa)) match {
+       case None => This(aa)
+       case Some(bb) => Both(aa, bb)
      })
 
-    left ++ b.filterNot(b => a.exists(aa => N.name(aa) == N.name(b) )).map(That(_))
+    val right =
+      b.filterNot(b => a.exists(aa => N.name(aa) == N.name(b) )).map(That(_))
+
+    left ++ right
   }
 
   implicit class DeltaOps[A](a: List[A])(implicit M: Delta[A], N: HasName[A]) {
@@ -119,32 +140,37 @@ object Main extends App {
   val ss = DamnThing(s)
   val ss2 = DamnThing(s2)
 
+  import Delta._
+
   val r1 = LabelledGeneric[DamnThing].to(ss)
   val r2 = LabelledGeneric[DamnThing].to(ss2)
 
-/*
-  object diffPoly extends LowPriorityInstances1 {
-    implicit def xWithProduct[K <: Symbol, V, V1K <: Symbol, V1V, R <: HList](
-     implicit W: Witness.Aux[K],
-     WV1K: Witness.Aux[V1K],
-     LB: LabelledGeneric.Aux[V, R]
-    ): Case.Aux[(FieldType[K, V], FieldType[K, V]), Delta.Meta] =
-      at{ case (a, b) => if (a == b) Map.empty else Map(W.value.name -> ((a: V).toString -> (b: V).toString)) }
-  }
+ val r: List[DeltaOp[MajorResource1]] = List(MajorResource1(???, ???), MajorResource1(???, ???)).diffWith(
+   List(MajorResource1(???, ???), MajorResource1(???, ???))
+ )
+  /*
+    object diffPoly extends LowPriorityInstances1 {
+      implicit def xWithProduct[K <: Symbol, V, V1K <: Symbol, V1V, R <: HList](
+       implicit W: Witness.Aux[K],
+       WV1K: Witness.Aux[V1K],
+       LB: LabelledGeneric.Aux[V, R]
+      ): Case.Aux[(FieldType[K, V], FieldType[K, V]), Delta.Meta] =
+        at{ case (a, b) => if (a == b) Map.empty else Map(W.value.name -> ((a: V).toString -> (b: V).toString)) }
+    }
 
-  trait LowPriorityInstances1 extends Poly1 {
-    implicit def diffAllLabelledTypes[K <: Symbol, V](
-         implicit W: Witness.Aux[K]
-       ): Case.Aux[(FieldType[K, V], FieldType[K, V]), Delta.Meta] =
-      at{ case (a, b) => if (a == b) Map.empty else Map(W.value.name -> ((a: V).toString -> (b: V).toString)) }
-  }
+    trait LowPriorityInstances1 extends Poly1 {
+      implicit def diffAllLabelledTypes[K <: Symbol, V](
+           implicit W: Witness.Aux[K]
+         ): Case.Aux[(FieldType[K, V], FieldType[K, V]), Delta.Meta] =
+        at{ case (a, b) => if (a == b) Map.empty else Map(W.value.name -> ((a: V).toString -> (b: V).toString)) }
+    }
 
 
- object FoldPo extends Poly2 {
-    implicit val foldAllMeta: Case.Aux[Delta.Meta, Delta.Meta, Delta.Meta] =
-      at(_ |+| _)
-  }
-*/
+   object FoldPo extends Poly2 {
+      implicit val foldAllMeta: Case.Aux[Delta.Meta, Delta.Meta, Delta.Meta] =
+        at(_ |+| _)
+    }
+  */
 
   // .foldLeft(Map.empty[String, (String, String)])(FoldPo)
   //println(r1.zip(r2).map(diffPoly))
